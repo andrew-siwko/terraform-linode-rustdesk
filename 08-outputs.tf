038cd45ceecbbe737d3853f01a2e099c0f18cf92
Andrew Siwko <asiwko@siwko.org>
2026-07-26 19:09:10 -0400

    remove unused outputs

--- 08-outputs.tf
+++ 08-outputs.tf
@@ -1,13 +1,3 @@
 output "linode_instance_public_ip" {
   value   = one(linode_instance.asiwko-rustdesk-01.ipv4)
 }
-
-# output "machine_info-01" {
-#   value = {
-#     id     = linode_instance.asiwko-rustdesk-01.id
-#     label  = linode_instance.asiwko-rustdesk-01.label
-#     ipv4   = one(linode_instance.asiwko-rustdesk-01.ipv4)
-#     ipv6   = linode_instance.asiwko-rustdesk-01.ipv6
-#     region = linode_instance.asiwko-rustdesk-01.region
-#   }
-# }
